3fad682e4a890ba9b82b6d3178e88bb42618b034
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-10-25 19:34:16 -0400

    finish 11, end= apost tell print to not end the line with a newlince character

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("how old are you?", end =' ')
+age = input()
+print("how tall are you?")
+height = input()
+print("how much do you weigh", end=' ')
+weight = input()
+
+
+print(f"so, youre {age} old, {height} tall and {weight} heavy.")
