7ca9089beb6d4bb7e6397385f45971e8d7881d57
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-10-24 23:47:50 -0400

    done ex5

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'zed'
+my_age = 25 # not a lie
+my_height = 74 #inches
+my_weight = 180 # lbs
+my_eyes = 'blue' #color
+my_teeth = 'white'
+my_hair = 'brown'
+
+print(f"let's talk about {my_name}.")
+print(f"he's {my_height} inches tall")
+print(f"he's {my_weight} pounds heavy")
+print("actually thats not too heavy")
+print(f"he's go {my_eyes} eyes and {my_hair} hair.")
+print(f"his teeth are usually {my_teeth} depending on the coffee")
+
+#this line is tricky, try to get it exactly right
+
+total = my_age / 10 + my_height / 10 + my_weight / 10
+print(f"if I add {my_age / 10}, {my_height / 10}, and {my_weight / 10} I get {total}")
