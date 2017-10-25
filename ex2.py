c13d7b983c36803fd3f3ca5f7bc23d032c60dc87
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-10-24 21:00:06 -0400

    done ex2

---
+++ ex2.py
@@ -0,0 +1,6 @@
+# a comment, this is so you can ready your program later
+#anything after #is igrnored by python
+
+print("I could have code like this.") # and the comment after is iirnored
+# you can use a comment to disable or comment out code
+print("this will run")
